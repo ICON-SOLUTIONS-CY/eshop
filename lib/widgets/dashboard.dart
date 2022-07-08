7e2444c9012acf1e199e8b04e3383da1e12d6f29
ioslbj <ioannisrodosthenous@gmail.com>
2022-07-08 12:45:56 +0300

    Implementation of CategoryHeader Widget

--- lib/widgets/dashboard.dart
+++ lib/widgets/dashboard.dart
@@ -1,6 +1,5 @@
-
 import 'package:flutter/material.dart';
-import 'package:eshop/utils/extensions.dart';
+import 'categoryHeader.dart';
 import 'header.dart';
 
 class Dashboard extends StatefulWidget {
@@ -13,6 +12,7 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+
   @override
   build(context) => SafeArea(
         child: Scaffold(
@@ -26,42 +26,7 @@ class _DashboardState extends State<Dashboard> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   HeaderWidget(),
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: context.screenSize.width * .05
-                    ),
-                    child: SizedBox(
-                      height: 56,
-                      width: double.infinity,
-                      child: ListView.builder(
-                        itemCount: categoryList.length,
-                        scrollDirection: Axis.horizontal,
-                        physics: const ScrollPhysics(),
-                        itemBuilder: (_, index) => Container(
-                          width: 144,
-                          height: 50,
-                          margin: const EdgeInsets.symmetric(horizontal: 3),
-                          decoration: const BoxDecoration(
-                            color: Color(0xFFEBF2FF),
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(15),
-                            )
-                          ),
-                          child: Column(
-                            children: [
-                              /*SvgPicture.asset(
-                                  categoryList[index]['icon'],
-                                ),*/
-                              const SizedBox(
-                                height: 5,
-                              ),
-                              Text(categoryList[index]['title']),
-                            ]
-                          )
-                        )
-                      )
-                    )
-                  )
+                  const CategoryHeader(),
                 ]
               )
             )
